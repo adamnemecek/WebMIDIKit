97ce887c09e3f462623db65d7e4e3212b8f56bd6
Adam Nemecek <adamnemecek@gmail.com>
2016-12-13 11:37:54 -0800

    initial commit

---
+++ Sources/MIDIPort.swift
@@ -0,0 +1,244 @@
+//
+//  WebMIDI.swift
+//  WarpKit
+//
+//  Created by Adam Nemecek on 12/6/16.
+//
+//
+
+import Foundation
+import AVFoundation
+
+fileprivate func MIDIObjectGetStringProperty(ref: MIDIObjectRef, property: CFString) -> String {
+    var string: Unmanaged<CFString>? = nil
+    MIDIObjectGetStringProperty(ref, property, &string)
+    return (string?.takeRetainedValue())! as String
+}
+
+fileprivate func MIDIObjectGetIntProperty(ref: MIDIObjectRef, property: CFString) -> Int {
+    var val: Int32 = 0
+    MIDIObjectGetIntegerProperty(ref, property, &val)
+    return Int(val)
+}
+
+fileprivate func MIDIObjectGetType(id: Int) -> MIDIObjectType {
+    var ref: MIDIObjectRef = 0
+    var type: MIDIObjectType = .other
+    MIDIObjectFindByUniqueID(MIDIUniqueID(id), &ref, &type)
+    return type
+}
+
+
+enum MIDIPortType: Equatable {
+    case input, output
+
+//    fileprivate init<Port: WebMIDIPort>(port: Port) {
+//        if let _ =  port as? WebMIDIInput {
+//            self = .input
+//        }
+//        else if let _ = port as? WebMIDIOutput {
+//            self = .output
+//        }
+//        else {
+//            fatalError("")
+//        }
+//    }
+
+    fileprivate init(type: MIDIObjectType) {
+
+        switch type {
+            case .source:
+                self = .input
+            case .destination:
+                self = .output
+            default:
+                fatalError("invalud type \(type)")
+        }
+    }
+}
+
+
+enum MIDIPortDeviceState: Equatable {
+    case disconnected, connected
+}
+
+enum MIDIPortConnectionState: Equatable {
+    case open, closed, pending
+}
+
+class WebMIDIPort: Comparable, Hashable, CustomStringConvertible {
+    private let ref: MIDIPortRef
+
+    private weak var client: WebMIDIClient? = nil
+
+    internal init(client: WebMIDIClient?, ref: MIDIPortRef) {
+        self.ref = ref
+
+        _ = client.map {
+            self.client = $0
+            fatalError("connect")
+        }
+    }
+
+    internal init?(client: WebMIDIClient, where: (WebMIDIPort) -> Bool) {
+        fatalError()
+    }
+
+    deinit {
+        _ = self.client.map {
+            MIDIPortDisconnectSource(ref, $0.ref)
+            fatalError("")
+        }
+//        MIDIPortDispose(ref)
+    }
+
+    var hashValue: Int {
+        return ref.hashValue
+    }
+
+    private subscript(string property: CFString) -> String {
+        return MIDIObjectGetStringProperty(ref: ref, property: property)
+    }
+
+
+    private subscript(int property: CFString) -> Int {
+        return MIDIObjectGetIntProperty(ref: ref, property: property)
+    }
+
+    var id: Int {
+        return self[int: kMIDIPropertyUniqueID]
+    }
+
+    var manufacturer: String {
+        return self[string: kMIDIPropertyManufacturer]
+    }
+
+    var name: String {
+        return self[string: kMIDIPropertyDisplayName]
+    }
+
+    var version: Int {
+        return self[int: kMIDIPropertyDriverVersion]
+    }
+
+    var state: MIDIPortDeviceState {
+        fatalError()
+    }
+
+    var type: MIDIPortType {
+        return MIDIPortType(type: MIDIObjectGetType(id: id))
+    }
+
+    var connection: MIDIPortConnectionState {
+        fatalError()
+    }
+
+    var description: String {
+        return ""
+    }
+
+    static func ==(lhs: WebMIDIPort, rhs: WebMIDIPort) -> Bool {
+        return lhs.ref == rhs.ref
+    }
+
+    static func <(lhs: WebMIDIPort, rhs: WebMIDIPort) -> Bool {
+        return lhs.ref < rhs.ref
+    }
+
+
+    open func onstatechange(event: Int) {
+
+    }
+
+    func close() {
+        guard state != .disconnected else { return }
+        fatalError()
+    }
+}
+
+final class WebMIDIInput: WebMIDIPort {
+
+    internal init(client: WebMIDIClient, readmidi: @escaping (MIDIPacketList) -> ()) {
+        var ref = MIDIPortRef()
+
+        MIDIInputPortCreateWithBlock(client.ref, "MIDI input" as CFString, &ref) {
+            packetlist, srcconref in
+            readmidi(packetlist.pointee)
+        }
+        super.init(client: client, ref: ref)
+    }
+
+//    func onmidimessage(event: Int) {
+//
+//    }
+
+}
+
+extension Collection where Index == Int {
+    func index(after i: Index) -> Index {
+        return i + 1
+    }
+}
+
+struct WebMIDIInputMap: Collection {
+    typealias Index = Int
+    typealias Element = WebMIDIInput
+
+    var startIndex: Index {
+        return 0
+    }
+
+    var endIndex: Index {
+        return MIDIGetNumberOfSources()
+    }
+
+    subscript (index: Index) -> Element {
+//        return Element(ref: MIDIGetSource(index))
+        fatalError()
+    }
+}
+
+final class WebMIDIOutput: WebMIDIPort {
+//    init(name: String) {
+//        var ref: MIDIPortRef
+//        MIDIOutputPortCreate(0, name as CFString, &ref)
+//        super.init(ref: ref)
+//    }
+    init(client: WebMIDIClient) {
+        var ref = MIDIPortRef()
+        MIDIOutputPortCreate(client.ref, "MIDI output" as CFString, &ref)
+        super.init(client: client, ref: ref)
+    }
+
+    func send<S: Sequence>(data: S, timestamp: Int = 0) {
+
+    }
+
+    func clear() {
+
+    }
+
+}
+
+struct WebMIDIOutputMap {
+    typealias Index = Int
+    typealias Element = WebMIDIOutput
+
+    var startIndex: Index {
+        return 0
+    }
+
+    var endIndex: Index {
+        return MIDIGetNumberOfDestinations()
+    }
+
+    subscript (index: Index) -> Element {
+//        return Element(ref: MIDIGetDestination(index))
+        fatalError()
+    }
+}
+
+
+
+
+
--- Sources/WebMIDIKit.swift
+++ Sources/WebMIDIKit.swift
@@ -1,4 +1,196 @@
-struct WebMIDIKit {
+//
+//  WebMIDIManager.swift
+//  WarpKit
+//
+//  Created by Adam Nemecek on 12/7/16.
+//
+//
 
-    var text = "Hello, World!"
+import Foundation
+import AVFoundation
+
+
+
+//class Dispatcher<D: Delegate> {
+//    private let delegate : D
+//
+//    init(delegate: D) {
+//        self.delegate = delegate
+//    }
+//
+//    func observe(event: D.Event) {
+//        delegate.dispatch(event: event)
+//    }
+//}
+
+
+protocol MIDIClientType {
+    func add(port: WebMIDIPort)
+    func receiveMIDI(port: WebMIDIPort, packet: MIDIPacketList)
 }
+
+protocol Delegate {
+    associatedtype Event
+    func dispatch(event: Event)
+}
+
+internal final class WebMIDIClient: Comparable, Hashable {
+    private(set) var ref: MIDIClientRef = MIDIClientRef()
+
+    init() {
+        MIDIClientCreateWithBlock("webmidi" as CFString, &self.ref) {
+            self.dispatch(event: $0.pointee)
+        }
+    }
+
+    deinit {
+        MIDIClientDispose(ref)
+    }
+
+    fileprivate func dispatch(event: MIDINotification) {
+        switch event.messageID {
+        case .msgObjectAdded:
+            break
+        default:
+            break
+        }
+    }
+
+    var hashValue: Int {
+        return ref.hashValue
+    }
+
+    static func ==(lhs: WebMIDIClient, rhs: WebMIDIClient) -> Bool {
+        return lhs.ref == rhs.ref
+    }
+
+    static func <(lhs: WebMIDIClient, rhs: WebMIDIClient) -> Bool {
+        return lhs.ref < rhs.ref
+    }
+}
+
+fileprivate struct MIDIInputs {
+    typealias Index = Int
+    typealias Element = MIDIPortRef
+
+    var startIndex: Index {
+        return 0
+    }
+
+    var endIndex: Index {
+        return MIDIGetNumberOfDestinations()
+    }
+
+    subscript (index: Index) -> Element {
+//        return MIDI(index)
+        fatalError()
+    }
+}
+
+fileprivate struct MIDIOutputs: Collection {
+    typealias Index = Int
+    typealias Element = MIDIPortRef
+
+    var startIndex: Index {
+        return 0
+    }
+
+    var endIndex: Index {
+        return MIDIGetNumberOfDestinations()
+    }
+
+    subscript (index: Index) -> Element {
+        return MIDIGetDestination(index)
+    }
+}
+
+fileprivate struct MIDISources: Collection {
+    typealias Index = Int
+    typealias Element = MIDIEndpointRef
+
+    var startIndex: Index {
+        return 0
+    }
+
+    var endIndex: Index {
+        return MIDIGetNumberOfSources()
+    }
+
+    subscript (index: Index) -> Element {
+        return MIDIGetSource(index)
+    }
+}
+
+
+fileprivate struct MIDIDestinations: Collection {
+    typealias Index = Int
+    typealias Element = MIDIEndpointRef
+
+    var startIndex: Index {
+        return 0
+    }
+
+    var endIndex: Index {
+        return MIDIGetNumberOfDestinations()
+    }
+
+    subscript (index: Index) -> Element {
+        return MIDIGetDestination(index)
+    }
+}
+
+
+class WebMIDIManager {
+    private let clients: Set<WebMIDIClient> = []
+
+    let inputs: [WebMIDIInput] = []
+    let outputs: [WebMIDIOutput] = []
+
+//    var input: WebMIDIInput {
+//        return WebMIDIInput()
+//    }
+//
+//    var output: WebMIDIOutput {
+//        fatalError()
+//    }
+}
+
+final class WebMIDIManagerMac: WebMIDIManager {
+    let client: WebMIDIClient
+
+    let input: WebMIDIInput
+    let output: WebMIDIOutput
+
+    let destinations: [MIDIEndpointRef]
+    let sources: [MIDIEndpointRef]
+
+    override init() {
+        client = WebMIDIClient()
+
+        input = WebMIDIInput(client: client) {
+            _ in
+        }
+
+        output = WebMIDIOutput(client: client)
+        destinations = Array(MIDIDestinations())
+        sources = MIDISources().map { source in
+
+//            MIDIPortConnectSource(MIDIPortRef, MIDIEndpointRef, UnsafeMutableRawPointer?)
+//            MIDIPortConnectSource(input.r, <#T##source: MIDIEndpointRef##MIDIEndpointRef#>, <#T##connRefCon: UnsafeMutableRawPointer?##UnsafeMutableRawPointer?#>)
+            return source
+        }
+
+
+
+        super.init()
+
+
+    }
+}
+
+
+
+
+
+
+
