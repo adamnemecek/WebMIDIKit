bdcc861586b3e42215b85264adf94ed0063e51a3
Adam Nemecek <adamnemecek@gmail.com>
2017-01-31 16:27:54 -0800

    * axmidi value get optimization  (uses pointer)

--- Sources/AXMIDI/AXMIDI.c
+++ Sources/AXMIDI/AXMIDI.c
@@ -8,7 +8,7 @@
 
 #include "AXMIDI.h"
 
-Byte MIDIPacketGetValue(MIDIPacket packet, int index) {
+Byte MIDIPacketGetValue(const MIDIPacket packet, int index) {
 	return packet.data[index];
 }
 
@@ -18,8 +18,16 @@ void MIDIPacketSetValue(MIDIPacket * const packet, int index, Byte value) {
 
 MIDIPacket MIDIPacketCreate(MIDITimeStamp timestamp, const Byte* data, int count) {
   MIDIPacket p = {.timeStamp = timestamp, .length = count};
+  //  memcpy(p.data, data, count/(sizeof(void*)/sizeof(Byte)));
+  //todo memcpy
   for (int i = 0; i < count; i++) {
     p.data[i] = data[i];
   }
+
   return p;
 }
+
+//const MIDIPacket* MIDIPacketNext2(const MIDIPacket* packet) {
+//  return MIDIPacketNext(packet);
+//}
+
--- Sources/AXMIDI/include/AXMIDI.h
+++ Sources/AXMIDI/include/AXMIDI.h
@@ -8,7 +8,14 @@
 
 #import <CoreMIDI/CoreMIDI.h>
 
+//
+// Note that marking packet as const 
+//
 Byte MIDIPacketGetValue(const MIDIPacket packet, int index);
 void MIDIPacketSetValue(MIDIPacket* const packet, int index, Byte value);
 
 MIDIPacket MIDIPacketCreate(MIDITimeStamp timestamp, const Byte* data, int count);
+
+//const MIDIPacket* MIDIPacketNext2(const MIDIPacket* packet);
+//void MIDIPacketRangeReplace(NSRange range, Byte* with, int count);
+//MIDIPacketList MIDIPacketListCreate(const MIDIPacket* lst, int count);
