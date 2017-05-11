ef469a2c7e3871a6e01f116e106f21c9ade094af
Adam Nemecek <adamnemecek@gmail.com>
2017-05-11 07:38:13 -0700

    * packets

--- Sources/WebMIDIKit/MIDIPacketList.swift
+++ Sources/WebMIDIKit/MIDIPacketList.swift
@@ -11,8 +11,7 @@ import AVFoundation
 extension MIDIPacketList {
     /// this needs to be mutating since we are potentionally changint the timestamp
     /// we cannot make a copy since that woulnd't copy the whole list
-    mutating
-    func send(to output: MIDIOutput, offset: Double? = nil) {
+    internal mutating func send(to output: MIDIOutput, offset: Double? = nil) {
         _ = offset.map {
             let current = AudioGetCurrentHostTime()
             let _offset = AudioConvertNanosToHostTime(UInt64($0 * 1000000))
@@ -38,13 +37,28 @@ extension MIDIPacketList {
     }
 }
 
+extension Data {
+    fileprivate var bytes : UnsafeRawPointer {
+        return (self as NSData).bytes
+    }
+}
+
 extension MIDIPacket {
-    init(_ data: [UInt8]) {
+    init(_ data: [UInt8], timestamp: MIDITimeStamp = 0) {
         self.init()
-        self.timeStamp = 0
+        self.timeStamp = timestamp
         self.length = UInt16(data.count)
         _ = withUnsafeMutableBytes(of: &self.data) {
             memcpy($0.baseAddress, data, data.count)
+        }
+    }
+
+    init(_ data: Data, timestamp: MIDITimeStamp = 0) {
+        self.init()
+        self.timeStamp = timestamp
+        self.length = UInt16(data.count)
+        _ = withUnsafeMutableBytes(of: &self.data) {
+            memcpy($0.baseAddress, data.bytes, data.count)
         }
     }
 }
