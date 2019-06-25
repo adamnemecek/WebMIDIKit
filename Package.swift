cc8b1d3eed7d296a675b6e74ce9042af96bcf6db
Niklas Kleemann <n.kleemann@posteo.de>
2019-06-26 01:38:40 +0200

    Cleanup

--- Package.swift
+++ Package.swift
@@ -6,18 +6,13 @@ import PackageDescription
 let package = Package(
     name: "WebMIDIKit",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "WebMIDIKit",
             targets: ["WebMIDIKit"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target defines a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "WebMIDIKit",
             dependencies: []),
@@ -26,12 +21,3 @@ let package = Package(
             dependencies: []),
     ]
 )
-
-//import PackageDescription
-//
-//let package = Package(
-//    name: "WebMIDIKit",
-//    targets: [
-//        Target(name: "WebMIDIKit")
-//    ]
-//)
