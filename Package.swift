d54f30c19c8448b05ba90d434f5a4f126def13ad
Niklas Kleemann <n.kleemann@posteo.de>
2019-06-26 01:55:07 +0200

    Conform to new SPM syntax

--- Package.swift
+++ Package.swift
@@ -1,8 +1,23 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "WebMIDIKit",
+    products: [
+        .library(
+            name: "WebMIDIKit",
+            targets: ["WebMIDIKit"]),
+    ],
+    dependencies: [
+    ],
     targets: [
-      Target(name: "WebMIDIKit")
+        .target(
+            name: "WebMIDIKit",
+            dependencies: []),
+        .testTarget(
+            name: "WebMIDIKitTests",
+            dependencies: []),
     ]
 )
